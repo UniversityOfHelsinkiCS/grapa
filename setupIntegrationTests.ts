c5e2abc0b2ba8c41d0816638fc70253269a81d7c
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-05-14 17:02:38 +0300

    Attempting better integration test setup

--- setupIntegrationTests.ts
+++ setupIntegrationTests.ts
@@ -1,13 +1,40 @@
+import supertest from 'supertest'
 import { jest } from '@jest/globals'
 import * as db from './src/server/db/connection'
+import app from './src/server/index'
+
+// eslint-disable-next-line no-promise-executor-return
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
 global.jest = jest
+
+// eslint-disable-next-line import/no-mutable-exports
+let request: any = null
 
 global.beforeEach(async () => {
   // retry to connect to the DB in case the test runner starts before the DB is ready
   await db.connectToDatabase(3)
+  request = supertest(app)
+
+  let response: any = null
+  let retryCount = 0
+  while (retryCount < 3) {
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      response = await request.get('/api/theses')
+      expect(response.status).toEqual(200)
+      break
+    } catch (error) {
+      retryCount += 1
+      // eslint-disable-next-line no-await-in-loop
+      await sleep(2000)
+    }
+  }
 }, 20000)
 
 global.afterEach(async () => {
   await db.resetDatabase()
 })
+
+// eslint-disable-next-line import/prefer-default-export
+export { request }
