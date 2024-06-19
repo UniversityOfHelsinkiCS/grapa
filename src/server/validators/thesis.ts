fb1546d3f1dc448f62fd441581f7651998b1dc0a
Henri Remonen <henri@remonen.fi>
2024-06-19 14:59:14 +0300

    feat: update backend validators to throw custom validation errors

--- src/server/validators/thesis.ts
+++ src/server/validators/thesis.ts
@@ -1,5 +1,6 @@
-import { ServerPostRequest, ServerPutRequest } from '@backend/types'
 import { NextFunction } from 'express'
+import { ServerPostRequest, ServerPutRequest } from '@backend/types'
+import CustomValidationError from '../errors/ValidationError'
 
 // eslint-disable-next-line import/prefer-default-export
 export const validateThesisData = (
@@ -10,24 +11,34 @@ export const validateThesisData = (
   const thesisData = req.body
 
   if (!thesisData.topic) {
-    throw new Error('Thesis title is required')
+    throw new CustomValidationError('Thesis title is required', {
+      topic: ['Thesis topic is required'],
+    })
   }
 
   if (!thesisData.supervisions || thesisData.supervisions.length === 0) {
-    throw new Error('At least one supervision is required')
+    throw new CustomValidationError('At least one supervision is required', {
+      supervisions: ['At least one supervision is required'],
+    })
   }
 
   if (!thesisData.authors || thesisData.authors.length === 0) {
-    throw new Error('At least one author is required')
+    throw new CustomValidationError('At least one author is required', {
+      authors: ['At least one author is required'],
+    })
   }
 
   if (!thesisData.graders || thesisData.graders.length === 0) {
-    throw new Error('At least one grader is required')
+    throw new CustomValidationError('At least one grader is required', {
+      graders: ['At least one grader is required'],
+    })
   }
 
   // primary grader must be set
   if (!thesisData.graders.some((grader) => grader.isPrimaryGrader)) {
-    throw new Error('Primary grader must be set')
+    throw new CustomValidationError('Primary grader must be set', {
+      graders: ['Primary grader must be set'],
+    })
   }
 
   // sum of supervision percentages must add up to 100
@@ -36,37 +47,53 @@ export const validateThesisData = (
     0
   )
   if (totalPercentage !== 100) {
-    throw new Error('Supervision percentages must add up to 100')
+    throw new CustomValidationError(
+      'Supervision percentages must add up to 100',
+      { supervisions: ['Supervision percentages must add up to 100'] }
+    )
   }
 
   const researchPlanFile = req.files.researchPlan
     ? req.files.researchPlan[0]
     : thesisData.researchPlan
   if (!researchPlanFile) {
-    throw new Error('Research plan is required')
+    throw new CustomValidationError('Research plan is required', {
+      researchPlan: ['Research plan is required'],
+    })
   }
 
   const waysOfWorkingFile = req.files.waysOfWorking
     ? req.files.waysOfWorking[0]
     : thesisData.waysOfWorking
   if (!waysOfWorkingFile) {
-    throw new Error('Ways of working is required')
+    throw new CustomValidationError('Ways of working is required', {
+      waysOfWorking: ['Ways of working is required'],
+    })
   }
 
   if (!thesisData.startDate) {
-    throw new Error('Start date is required')
+    throw new CustomValidationError('Start date is required', {
+      startDate: ['Start date is required'],
+    })
   }
 
   if (!thesisData.targetDate) {
-    throw new Error('Target date is required')
+    throw new CustomValidationError('Target date is required', {
+      targetDate: ['Target date is required'],
+    })
   }
 
   if (thesisData.startDate > thesisData.targetDate) {
-    throw new Error('Start date must be before target date')
+    throw new CustomValidationError('Start date must be before target date', {
+      startDate: ['Start date must be before target date'],
+      targetDate: ['Start date must be before target date'],
+    })
   }
 
   if (!thesisData.programId) {
-    throw new Error('Program is required')
+    throw new CustomValidationError('Program is required', {
+      programId: ['Program is required'],
+    })
   }
 
   next()
