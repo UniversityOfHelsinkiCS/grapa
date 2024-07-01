a14017b148e8a16ac62b78f1e671575b434fb6b6
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-07-01 11:11:27 +0300

    Fix BASE_PATH to allow own domain in stg deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { inStaging } from './src/config'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: inStaging ? '/grapa' : '/',
   server: {
     proxy: {
       '/api/': {
