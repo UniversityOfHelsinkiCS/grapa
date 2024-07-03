b8c58d19e46c0d10fdf7daa2f6f7bf3ba5067135
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-07-03 17:27:29 +0300

    Change logout endpoint to be POST instead of GET as recommended in the docs

--- src/server/routes/logout.ts
+++ src/server/routes/logout.ts
@@ -2,13 +2,13 @@ import express from 'express'
 
 const logoutRouter = express.Router()
 
-logoutRouter.get('/', async (req, res, next) => {
+logoutRouter.post('/', async (req, res, next) => {
   // eslint-disable-next-line consistent-return
   req.logout((err) => {
     if (err) return next(err)
-  })
 
-  res.status(200).end()
+    res.redirect('/')
+  })
 })
 
 export default logoutRouter
