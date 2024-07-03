8f86c3ac560eef4d642cc45878df670c35967dd5
Henri Remonen <henri@remonen.fi>
2024-07-03 10:47:33 +0300

    feat: add remove button for the secondary grader -refs: #45

--- src/client/components/ThesisPage/GraderSelect/GraderSelect.tsx
+++ src/client/components/ThesisPage/GraderSelect/GraderSelect.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next'
 import { ZodIssue } from 'zod'
 import SingleGraderSelect from './SingleGraderSelect'
 import NewPersonControls from '../NewPersonControls'
+import ExternalPersonInput from '../ExternalPerson'
 
 const GraderSelect: React.FC<{
   errors: ZodIssue[]
@@ -28,6 +29,14 @@ const GraderSelect: React.FC<{
     ]
 
     setGraderSelections(updatedSelections)
+  }
+
+  const handleRemoveGrader = (index: number) => {
+    const initialSelections = [...graderSelections]
+    initialSelections.splice(index, 1)
+    if (initialSelections.length === 0) return // Do not allow removing all graders
+
+    setGraderSelections(initialSelections)
   }
 
   const professorInputError = errors.find(
@@ -57,28 +66,73 @@ const GraderSelect: React.FC<{
         {t('thesisForm:graderInstructions:content')}
       </Alert>
 
-      {graderSelections.map((selection, index) => (
-        <SingleGraderSelect
-          key={selection.user?.id ?? `grader-${index}`}
-          index={index}
-          selection={selection}
-          handleGraderChange={(grader) => handleChange(index, grader)}
-          inputProps={{
-            required: index === 0,
-            helperText:
-              index === 0
-                ? t(
-                    professorInputError?.message ??
-                      'thesisForm:graderInstructions:primaryGrader'
-                  )
-                : t(
-                    phdInputError?.message ??
-                      'thesisForm:graderInstructions:secondaryGrader'
-                  ),
-            error: Boolean(index === 0 ? professorInputError : phdInputError),
-          }}
-        />
-      ))}
+      {graderSelections.map((selection, index) => {
+        const { isExternal } = selection
+
+        if (isExternal) {
+          return (
+            <ExternalPersonInput
+              key={selection.user?.id ?? `grader-${index}`}
+              index={index}
+              inputGroup="graders"
+              selection={selection}
+              handlePersonChange={(value) => handleChange(index, value)}
+              handleRemovePerson={() => handleRemoveGrader(index)}
+              inputErrors={{
+                firstName: t(
+                  errors.find(
+                    (error) =>
+                      error.path.join('-') === `graders-${index}-user-firstName`
+                  )?.message
+                ),
+                lastName: t(
+                  errors.find(
+                    (error) =>
+                      error.path.join('-') === `graders-${index}-user-lastName`
+                  )?.message
+                ),
+                email: t(
+                  errors.find(
+                    (error) =>
+                      error.path.join('-') === `graders-${index}-user-email`
+                  )?.message
+                ),
+              }}
+              inputProps={{
+                required: false,
+              }}
+            />
+          )
+        }
+
+        return (
+          <SingleGraderSelect
+            key={selection.user?.id ?? `grader-${index}`}
+            index={index}
+            selection={selection}
+            handleGraderChange={(grader) => handleChange(index, grader)}
+            handleRemoveGrader={() => handleRemoveGrader(index)}
+            inputProps={{
+              required: index === 0,
+              helperText:
+                index === 0
+                  ? t(
+                      professorInputError?.message ??
+                        'thesisForm:graderInstructions:primaryGrader'
+                    )
+                  : t(
+                      phdInputError?.message ??
+                        'thesisForm:graderInstructions:secondaryGrader'
+                    ),
+              error: Boolean(index === 0 ? professorInputError : phdInputError),
+            }}
+            iconButtonProps={{
+              disabled: index === 0,
+              sx: { display: index === 0 ? 'none' : 'block' },
+            }}
+          />
+        )
+      })}
 
       {graderSelections.length < 2 && (
         <NewPersonControls
--- src/client/components/ThesisPage/GraderSelect/SingleGraderSelect.tsx
+++ src/client/components/ThesisPage/GraderSelect/SingleGraderSelect.tsx
@@ -1,64 +1,103 @@
 import React from 'react'
 import {
   Autocomplete,
+  Box,
+  ButtonProps,
   FormControl,
+  IconButton,
+  Stack,
   TextField,
   TextFieldProps,
 } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
 import { User, GraderData } from '@backend/types'
 import { useTranslation } from 'react-i18next'
 import { useDebounce } from '../../../hooks/useDebounce'
 import useUsers from '../../../hooks/useUsers'
+import DeleteConfirmation from '../../Common/DeleteConfirmation'
 
 interface SingleGraderSelectProps {
   index: number
-  handleGraderChange: (value: User | null) => void
   selection: GraderData
+  handleGraderChange: (value: User | null) => void
+  handleRemoveGrader: () => void
   inputProps: TextFieldProps
+  iconButtonProps: ButtonProps
 }
 
 const SingleGraderSelect: React.FC<SingleGraderSelectProps> = ({
-  handleGraderChange,
-  selection,
   index,
+  selection,
+  handleGraderChange,
+  handleRemoveGrader,
   inputProps,
+  iconButtonProps,
 }) => {
   const { t } = useTranslation()
+  const [deleteDialogOpen, setDeleteDialogOpen] = React.useState(false)
   const [userSearch, setUserSearch] = React.useState('')
   const debouncedSearch = useDebounce(userSearch, 700)
   const { users } = useUsers({ search: debouncedSearch, onlyEmployees: true })
 
   return (
-    <FormControl fullWidth>
-      <Autocomplete<User>
-        id={`graders-${index}-user`}
-        noOptionsText={t('userSearchNoOptions')}
-        data-testid={`grader-select-input-${index + 1}`}
-        disablePortal
-        options={users ?? []}
-        getOptionLabel={(user) =>
-          `${user.firstName} ${user.lastName} ${user.email ? `(${user.email})` : ''} ${user.studentNumber ? `(${user.studentNumber})` : ''}`
-        }
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label={t('grader', { index: index + 1 })}
-            {...inputProps}
-          />
-        )}
-        inputValue={userSearch}
-        filterOptions={(x) => x}
-        isOptionEqualToValue={(option, value) => option.id === value.id}
-        value={selection.user ?? null}
-        onChange={(_, value) => handleGraderChange(value)}
-        onInputChange={(event, value) => {
-          // Fetch potential authors based on the input value
-          // You can use debounce or throttle to limit the number of requests
-          // Example: fetchPotentialAuthors(value)
-          setUserSearch(value)
-        }}
-      />
-    </FormControl>
+    <Stack spacing={1} direction="row">
+      <FormControl fullWidth>
+        <Autocomplete<User>
+          id={`graders-${index}-user`}
+          noOptionsText={t('userSearchNoOptions')}
+          data-testid={`grader-select-input-${index + 1}`}
+          disablePortal
+          options={users ?? []}
+          getOptionLabel={(user) =>
+            `${user.firstName} ${user.lastName} ${user.email ? `(${user.email})` : ''} ${user.studentNumber ? `(${user.studentNumber})` : ''}`
+          }
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label={t('grader', { index: index + 1 })}
+              {...inputProps}
+            />
+          )}
+          inputValue={userSearch}
+          filterOptions={(x) => x}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          value={selection.user ?? null}
+          onChange={(_, value) => handleGraderChange(value)}
+          onInputChange={(event, value) => {
+            // Fetch potential authors based on the input value
+            // You can use debounce or throttle to limit the number of requests
+            // Example: fetchPotentialAuthors(value)
+            setUserSearch(value)
+          }}
+        />
+      </FormControl>
+      <IconButton
+        data-testid="remove-grader-button"
+        type="button"
+        onClick={() => setDeleteDialogOpen(true)}
+        color="error"
+        size="small"
+        aria-label={`${t('removeButton')} ${selection.user?.firstName} ${selection.user?.lastName}`}
+        {...iconButtonProps}
+      >
+        <DeleteIcon />
+      </IconButton>
+      <DeleteConfirmation
+        open={deleteDialogOpen}
+        setOpen={setDeleteDialogOpen}
+        onClose={() => setDeleteDialogOpen(false)}
+        onDelete={handleRemoveGrader}
+        title={t('thesisForm:removeGraderConfirmationTitle')}
+      >
+        <Box>
+          {selection.user
+            ? t('thesisForm:removeGraderConfirmationContent', {
+                name: `${selection.user.firstName} ${selection.user.lastName}`,
+              })
+            : t('thesisForm:removeGraderConfirmationNoName')}
+        </Box>
+      </DeleteConfirmation>
+    </Stack>
   )
 }
 
