f951d2c805884ee2669af090f0c76b5800fba605
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-07-03 14:21:34 +0300

    Debug updater in prod

--- src/server/util/cron.ts
+++ src/server/util/cron.ts
@@ -2,12 +2,12 @@ import cron from 'node-cron'
 
 import logger from './logger'
 import { run as runUpdater } from '../updater'
-import { UPDATER_CRON_ENABLED, inDevelopment } from '../../config'
+import { UPDATER_CRON_ENABLED, inDevelopment, inProduction } from '../../config'
 
 const setupCron = async () => {
   logger.info('Starting cron jobs')
 
-  if (inDevelopment) {
+  if (inProduction || inDevelopment) {
     await runUpdater()
   } else if (UPDATER_CRON_ENABLED) {
     cron.schedule('45 0,12 * * *', runUpdater) // Run updater every 12 hours
