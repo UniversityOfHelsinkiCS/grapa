6132e94b27814bd922c98311d85e6c49b30762f4
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-07-08 11:28:36 +0300

    Use graylog transport for logger

--- src/server/util/logger.ts
+++ src/server/util/logger.ts
@@ -1,4 +1,6 @@
+import os from 'os'
 import winston from 'winston'
+import { WinstonGelfTransporter } from 'winston-gelf-transporter'
 
 import { inProduction } from '../../config'
 
@@ -39,6 +41,20 @@ if (!inProduction) {
     })
   )
   transports.push(new winston.transports.Console({ format: prodFormat }))
+
+  transports.push(
+    new WinstonGelfTransporter({
+      handleExceptions: true,
+      host: 'svm-116.cs.helsinki.fi',
+      port: 9503,
+      protocol: 'udp',
+      hostName: os.hostname(),
+      additional: {
+        app: 'grapa',
+        environment: 'production',
+      },
+    })
+  )
 }
 
 const logger = winston.createLogger({ transports })
