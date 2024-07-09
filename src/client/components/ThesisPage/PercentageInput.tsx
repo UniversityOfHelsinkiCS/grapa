c7fc47a22196bef8b3589413bb707c63ebebd419
Henri Remonen <henri@remonen.fi>
2024-07-09 12:20:02 +0300

    feat: create common component for the PercentageInput

---
+++ src/client/components/ThesisPage/PercentageInput.tsx
@@ -0,0 +1,33 @@
+import { TextField, InputAdornment, TextFieldProps } from '@mui/material'
+
+interface PercentageInputProps {
+  label: string
+  value: number
+  onChange: (percentage: number) => void
+  // eslint-disable-next-line react/require-default-props
+  percentageInputProps?: TextFieldProps
+}
+
+const PercentageInput = ({
+  label,
+  value,
+  onChange: handlePercentageChange,
+  percentageInputProps,
+}: PercentageInputProps) => (
+  <TextField
+    type="number"
+    sx={{ width: '12ch' }}
+    InputProps={{
+      inputProps: { min: 1, max: 100 },
+      endAdornment: <InputAdornment position="end">%</InputAdornment>,
+    }}
+    label={label}
+    value={value}
+    onChange={(event) =>
+      handlePercentageChange(parseInt(event.target.value, 10))
+    }
+    {...percentageInputProps}
+  />
+)
+
+export default PercentageInput
