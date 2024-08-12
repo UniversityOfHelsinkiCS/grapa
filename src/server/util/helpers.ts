66f8201c28371a7e1d72460b551c475b1aec4092
Henri Remonen <henri@remonen.fi>
2024-08-12 10:01:49 +0300

    refactor: remove unused helper function -refs: #94

--- src/server/util/helpers.ts
+++ src/server/util/helpers.ts
@@ -4,36 +4,6 @@ import { ThesisData, SupervisionData } from '@backend/types'
 export const getTotalPercentage = (supervisions: SupervisionData[]) =>
   supervisions.reduce((total, selection) => total + selection.percentage, 0)
 
-export const getEqualSupervisorSelectionWorkloads = (
-  numberOfSupervisors: number,
-  supervisorSelections: SupervisionData[]
-) => {
-  const defaultPercentage = (1 / numberOfSupervisors) * 100
-  const roundedDefaultPercentage = Math.floor(defaultPercentage)
-
-  const totalPercentage = roundedDefaultPercentage * numberOfSupervisors
-
-  const updatedSelections = supervisorSelections.map((selection, index) => {
-    // Check if the total percentage is less than 100%
-    if (index === 0 && totalPercentage < 100) {
-      // Adjust the first selection's percentage to make the total 100%
-      const difference = 100 - totalPercentage
-
-      return {
-        ...selection,
-        percentage: roundedDefaultPercentage + difference,
-      }
-    }
-
-    return {
-      ...selection,
-      percentage: roundedDefaultPercentage,
-    }
-  })
-
-  return updatedSelections
-}
-
 // Transforms the raw query data to suitably formatted data for the frontend
 export const transformThesisData = (thesisData: ThesisData[]) =>
   thesisData.map((thesis) => ({
