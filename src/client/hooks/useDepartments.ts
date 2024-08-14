87988bb7d55336671c515846324fc18b0aedc4ee
Henri Remonen <henri@remonen.fi>
2024-08-14 09:13:27 +0300

    feat: sort departments by name -refs: #85

--- src/client/hooks/useDepartments.ts
+++ src/client/hooks/useDepartments.ts
@@ -1,8 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
+import { useTranslation } from 'react-i18next'
+
+import { getSortedPrograms } from '../components/ThesisPage/util'
+
 import apiClient from '../util/apiClient'
+
 import { ProgramData as Program } from '../../server/types'
 
 const useDepartments = () => {
+  const { i18n } = useTranslation()
+  const { language } = i18n
+
   const queryKey = ['departments']
 
   const queryFn = async (): Promise<Program[]> => {
@@ -13,7 +21,7 @@ const useDepartments = () => {
 
   const { data: departments, ...rest } = useQuery({ queryKey, queryFn })
 
-  return { departments, ...rest }
+  return { departments: getSortedPrograms(departments, language), ...rest }
 }
 
 export default useDepartments
