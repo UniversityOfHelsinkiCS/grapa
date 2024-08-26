f20d69ab36da1dd6ef9b269cea805c85aa043b1d
Henri Remonen <henri@remonen.fi>
2024-08-26 10:20:12 +0300

    feat: add delete confirmation to the attachment deletion -refs: #106

--- src/client/components/ThesisPage/Dropzone/FilePreview.tsx
+++ src/client/components/ThesisPage/Dropzone/FilePreview.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Chip, Link } from '@mui/material'
 import UploadFileIcon from '@mui/icons-material/UploadFile'
 
 import { FileData } from '@backend/types'
+
+import DeleteConfirmation from '../../Common/DeleteConfirmation'
+
 import { BASE_PATH } from '../../../../config'
 
 interface FilePreviewProps {
@@ -9,22 +14,41 @@ interface FilePreviewProps {
   onDelete: () => void
 }
 
-const FilePreview = ({ file, onDelete }: FilePreviewProps) => (
-  <Chip
-    label={
-      'filename' in file ? (
-        <Link href={`${BASE_PATH}/api/attachments/${file.filename}`}>
-          {file.name}
-        </Link>
-      ) : (
-        file.name
-      )
-    }
-    icon={<UploadFileIcon />}
-    variant="outlined"
-    sx={{ maxWidth: 200 }}
-    onDelete={onDelete}
-  />
-)
+const FilePreview = ({ file, onDelete }: FilePreviewProps) => {
+  const { t } = useTranslation()
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false)
+
+  return (
+    <>
+      <Chip
+        label={
+          'filename' in file ? (
+            <Link href={`${BASE_PATH}/api/attachments/${file.filename}`}>
+              {file.name}
+            </Link>
+          ) : (
+            file.name
+          )
+        }
+        icon={<UploadFileIcon />}
+        variant="outlined"
+        sx={{ maxWidth: 200 }}
+        onDelete={() => setDeleteDialogOpen(true)}
+      />
+      <DeleteConfirmation
+        open={deleteDialogOpen}
+        setOpen={setDeleteDialogOpen}
+        onClose={() => setDeleteDialogOpen(false)}
+        onDelete={() => {
+          setDeleteDialogOpen(false)
+          onDelete()
+        }}
+        title={t('thesisForm:removeAppendixConfirmationTitle')}
+      >
+        {t('thesisForm:removeAppendixConfirmationContent', { name: file.name })}
+      </DeleteConfirmation>
+    </>
+  )
+}
 
 export default FilePreview
