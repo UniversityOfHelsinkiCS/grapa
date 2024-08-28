10c25217a9d3883396a64ad4b32b590eb09e411c
Henri Remonen <henri@remonen.fi>
2024-08-28 10:43:39 +0300

    feat(updater): update the runUpdater to also run the attainment routine -refs: #81

--- src/server/updater/index.ts
+++ src/server/updater/index.ts
@@ -4,11 +4,13 @@ import { fetchUsers } from './users'
 import { clearOffsets } from './util'
 import { fetchPrograms } from './programs'
 import { fetchStudyTracks } from './studyTracks'
+import { fetchThesesAttainments } from './attainments'
 
 const runUpdater = async () => {
   await fetchPrograms()
   await fetchStudyTracks()
   await fetchUsers()
+  await fetchThesesAttainments()
 }
 
 export const run = async () => {
