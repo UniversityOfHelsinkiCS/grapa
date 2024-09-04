10e496853d99964b55897f0fdb2b5a0a533e800e
Henri Remonen <henri@remonen.fi>
2024-09-04 12:49:01 +0300

    test(department-admin): update integration tests to check for the new /statistics route -refs: #44

--- src/server/routes/departmentAdmins.integration-test.js
+++ src/server/routes/departmentAdmins.integration-test.js
@@ -1,6 +1,6 @@
 import supertest from 'supertest'
 import app from '../index'
-import { Department, DepartmentAdmin, User } from '../db/models'
+import { Thesis, Supervision, Program, StudyTrack, Department, DepartmentAdmin, User } from '../db/models'
 
 const request = supertest.agent(app)
 
@@ -27,6 +27,7 @@ describe('department-admins router', () => {
       lastName: 'test1',
       email: 'test@test.fi',
       language: 'fi',
+      departmentId: department1.id,
     })
     user2 = await User.create({
       username: 'test2',
@@ -34,6 +35,7 @@ describe('department-admins router', () => {
       lastName: 'test2',
       email: 'test@test.test2',
       language: 'fi',
+      departmentId: department2.id,
     })
     user3 = await User.create({
       username: 'test3',
@@ -41,6 +43,7 @@ describe('department-admins router', () => {
       lastName: 'test3',
       email: 'test@test.test3',
       language: 'fi',
+      departmentId: department1.id,
     })
 
     departmentAdmin1 = await DepartmentAdmin.create({
@@ -58,6 +61,13 @@ describe('department-admins router', () => {
     describe('GET /api/department-admins', () => {
       it('should return 403', async () => {
         const res = await request.get('/api/department-admins')
+        expect(res.status).toBe(403)
+      })
+    })
+
+    describe('GET /api/department-admins/statistics', () => {
+      it('should return 403', async () => {
+        const res = await request.get('/api/department-admins/statistics')
         expect(res.status).toBe(403)
       })
     })
@@ -91,6 +101,13 @@ describe('department-admins router', () => {
           .set({ uid: user3.id, hygroupcn: 'hy-employees' })
         expect(res.status).toBe(200)
         expect(res.body).toMatchObject([])
+      })
+    })
+
+    describe('GET /api/department-admins/statistics', () => {
+      it('should return 403 when the user is not a department admin', async () => {
+        const res = await request.get('/api/department-admins/statistics').set({ uid: user3.id, hygroupcn: 'hy-employees' })
+        expect(res.status).toBe(403)
       })
     })
 
@@ -199,6 +216,83 @@ describe('department-admins router', () => {
             userId: user3.id,
             user: expect.any(Object),
             department: expect.any(Object),
+          },
+        ])
+      })
+    })
+
+    describe('GET /api/department-admins/statistics', () => {
+      let thesis1
+
+      beforeEach(async () => {
+        await Program.create({
+          id: 'Testing program',
+          name: {
+            fi: 'Testausohjelma',
+            en: 'Testing program',
+            sv: 'Testprogram',
+          },
+          level: 'master',
+          international: true,
+          enabled: true,
+        }) 
+
+        await StudyTrack.create({
+          id: 'test-study-track-id',
+          programId: 'Testing program',
+          name: {
+            fi: 'Test study track',
+            en: 'Test study track',
+            sv: 'Test study track',
+          },
+        })
+
+        thesis1 = await Thesis.create({
+          programId: 'Testing program',
+          studyTrackId: 'test-study-track-id',
+          topic: 'test topic',
+          status: 'PLANNING',
+          startDate: '1970-01-01',
+          targetDate: '2070-01-01',
+        })
+
+        await Supervision.create({
+          userId: user1.id,
+          thesisId: thesis1.id,
+          percentage: 50,
+          isPrimarySupervisor: true,
+        })
+        await Supervision.create({
+          userId: user2.id,
+          thesisId: thesis1.id,
+          percentage: 50,
+          isPrimarySupervisor: false,
+        })
+      })
+
+      it('should return 200 and the statistics of the department admins', async () => {
+        const res = await request.get('/api/department-admins/statistics').set({ uid: user3.id, hygroupcn: 'hy-employees' })
+        expect(res.status).toBe(200)
+        expect(res.body).toIncludeSameMembers([
+          {
+            department: {
+              id: department1.id,
+              name: department1.name,
+            },
+            supervisor: {
+              id: user1.id,
+              username: user1.username,
+              firstName: user1.firstName,
+              lastName: user1.lastName,
+              email: user1.email,
+              departmentId: user1.departmentId,
+            },
+            statusCounts: {
+              PLANNING: 1,
+              IN_PROGRESS: 0,
+              COMPLETED: 0,
+              CANCELLED: 0,
+            },
           },
         ])
       })
