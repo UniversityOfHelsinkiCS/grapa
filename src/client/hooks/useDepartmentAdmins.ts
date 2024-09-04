a9416b22bc29948334d067c9683bb9cf87ab61a4
Henri Remonen <henri@remonen.fi>
2024-09-04 08:28:22 +0300

    feat: create query hook for fetching department admin statistics -refs: #44

--- src/client/hooks/useDepartmentAdmins.ts
+++ src/client/hooks/useDepartmentAdmins.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { DepartmentAdminData } from '@backend/types'
+import { DepartmentAdminData, ThesisStatistics } from '@backend/types'
 import apiClient from '../util/apiClient'
 
 const useDepartmentAdmins = () => {
@@ -14,6 +14,23 @@ const useDepartmentAdmins = () => {
   const { data: departmentAdmins, ...rest } = useQuery({ queryKey, queryFn })
 
   return { departmentAdmins, ...rest }
+}
+
+export const useDepartmentStatistics = () => {
+  const queryKey = ['department-admins', 'statistics']
+
+  const queryFn = async (): Promise<ThesisStatistics[]> => {
+    const { data } = await apiClient.get(`/department-admins/statistics`)
+
+    return data
+  }
+
+  const { data: departmentStatistics, ...rest } = useQuery({
+    queryKey,
+    queryFn,
+  })
+
+  return { departmentStatistics, ...rest }
 }
 
 export default useDepartmentAdmins
