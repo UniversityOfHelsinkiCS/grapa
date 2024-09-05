991681a699715f24e5bdb08b8540a43be39e7958
Henri Remonen <henri@remonen.fi>
2024-09-05 13:06:17 +0300

    test(department): add department admin role tests for the department route

--- src/server/routes/department.integration-test.js
+++ src/server/routes/department.integration-test.js
@@ -1,6 +1,6 @@
 import supertest from 'supertest'
 import app from '../index'
-import { User } from '../db/models'
+import { Department, DepartmentAdmin, User } from '../db/models'
 
 const request = supertest.agent(app)
 
@@ -68,6 +68,46 @@ describe('department router', () => {
   })
 
   describe('when the user is a department admin', () => {
-  
+    let user2
+    let department1
+
+    beforeEach(async () => {
+      department1 = await Department.findOne()
+
+      user2 = await User.create({
+        username: 'test2',
+        firstName: 'test2',
+        lastName: 'test2',
+        email: 'test2@test.fi',
+        departmentId: department1.id,
+        language: 'fi',
+      })
+
+      await DepartmentAdmin.create({
+        userId: user2.id,
+        departmentId: department1.id,
+      })
+    })
+
+    it('should return 200 and return every department when includeNotManaged is true', async () => {
+      // We are seeding the database with the six departments on one of the migrations
+      const res = await request
+        .get('/api/departments?includeNotManaged=true')
+        .set({ uid: user2.id, hygroupcn: 'hy-employees' })
+      expect(res.status).toBe(200)
+      expect(res.body).toHaveLength(6)
+    })
+
+    it('should return 200 and return only the managed departments when includeNotManaged is false', async () => {
+      const res = await request
+        .get('/api/departments?includeNotManaged=false')
+        .set({ uid: user2.id, hygroupcn: 'hy-employees' })
+      expect(res.status).toBe(200)
+      expect(res.body).toHaveLength(1)
+      expect(res.body).toIncludeSameMembers([{
+        id: department1.id,
+        name: department1.name,
+      }])
+    })
   })
 })
