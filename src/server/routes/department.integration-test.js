65c47eb8fdc1287914fbc55617e706729dfb61e0
Henri Remonen <henri@remonen.fi>
2024-09-09 08:13:49 +0300

    test(department): update test assertions

--- src/server/routes/department.integration-test.js
+++ src/server/routes/department.integration-test.js
@@ -7,6 +7,57 @@ const request = supertest.agent(app)
 describe('department router', () => {
   let user1
 
+  const departments = [
+    {
+      "id": "202ab77b-9c57-4cd0-a560-25dcd5108d6f", 
+      "name": {
+        "en": "Department of Chemistry", 
+        "fi": "Kemian osasto", 
+        "sv": "Avdelningen för kemi"
+      }
+    }, 
+    {
+      "id": "8fdce98e-2e9e-4cd7-a4c3-6d0176e29d37", 
+      "name": {
+        "en": "Department of Computer Science", 
+        "fi": "Tietojenkäsittelytieteen osasto", 
+        "sv": "Avdelningen för datavetenskap"
+      }
+    }, 
+    {
+      "id": "163a5a00-4571-42ac-89a6-7497dee73cbb", 
+      "name": {
+        "en": "Department of Geosciences and Geography", 
+        "fi": "Geotieteiden ja maantieteen osasto", 
+        "sv": "Avdelningen för geovetenskaper och geografi"
+      }
+    }, 
+    {
+      "id": "c0ce8ca5-6022-471b-a822-5172b1de507d", 
+      "name": {
+        "en": "Department of Mathematics and Statistics", 
+        "fi": "Matematiikan ja tilastotieteen osasto", 
+        "sv": "Avdelningen för matematik och statistik"
+      }
+    }, 
+    {
+      "id": "ad8a7f5f-2a54-496a-a30e-c0e512508b5d", 
+      "name": {
+        "en": "Department of Physics", 
+        "fi": "Fysiikan osasto", 
+        "sv": "Avdelningen för fysik"
+      }
+    }, 
+    {
+      "id": "305d14cc-6011-4861-b0af-1c9cccb38113", 
+      "name": {
+        "en": "Other", 
+        "fi": "Muu", 
+        "sv": "Övrig"
+      }
+    },
+  ]
+
   beforeEach(async () => {
     user1 = await User.create({
       username: 'test1',
@@ -33,7 +84,7 @@ describe('department router', () => {
         .get('/api/departments?includeNotManaged=true')
         .set({ uid: user1.id, hygroupcn: 'hy-employees' })
       expect(res.status).toBe(200)
-      expect(res.body).toHaveLength(6)
+      expect(res.body).toEqual(expect.arrayContaining(departments))
     })
 
     it('should return 200 and return only the managed departments when includeNotManaged is false', async () => {
@@ -53,7 +104,7 @@ describe('department router', () => {
         .get('/api/departments?includeNotManaged=true')
         .set({ uid: user1.id, hygroupcn: 'grp-toska' })
       expect(res.status).toBe(200)
-      expect(res.body).toHaveLength(6)
+      expect(res.body).toEqual(expect.arrayContaining(departments))
     })
 
     it('should return 200 and return only the managed departments when includeNotManaged is false', async () => {
@@ -62,7 +113,7 @@ describe('department router', () => {
         .get('/api/departments?includeNotManaged=false')
         .set({ uid: user1.id, hygroupcn: 'grp-toska' })
       expect(res.status).toBe(200)
-      expect(res.body).toHaveLength(6)
+      expect(res.body).toEqual(expect.arrayContaining(departments))
     })
 
   })
@@ -95,7 +146,7 @@ describe('department router', () => {
         .get('/api/departments?includeNotManaged=true')
         .set({ uid: user2.id, hygroupcn: 'hy-employees' })
       expect(res.status).toBe(200)
-      expect(res.body).toHaveLength(6)
+      expect(res.body).toEqual(expect.arrayContaining(departments))
     })
 
     it('should return 200 and return only the managed departments when includeNotManaged is false', async () => {
