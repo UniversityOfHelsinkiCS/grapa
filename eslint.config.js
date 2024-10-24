6bcf2a0218c4bd3581b54dfedcd31389c1fa153b
Aleksandr Tereshchenko <aleksandr.tereshchenko@helsinki.fi>
2024-10-24 13:25:36 +0300

    Update eslint-plugin-prettier, enable it's usage via eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,15 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  { rules: { '@typescript-eslint/no-explicit-any': 'off' } },
   pluginReact.configs.flat['jsx-runtime'],
-  {rules: { "@typescript-eslint/no-explicit-any": "off" }}
+  eslintPluginPrettierRecommended,
+  
 ]
