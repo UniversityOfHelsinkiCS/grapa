fdb64b646c74e2c35ffeea4d58f425c7dd40da5b
Aleksandr Tereshchenko <aleksandr.tereshchenko@helsinki.fi>
2024-11-04 15:29:33 +0200

    usePrograms: Send lang as query param instead of sorting inside the hook

--- src/client/hooks/usePrograms.ts
+++ src/client/hooks/usePrograms.ts
@@ -2,7 +2,6 @@ import { useTranslation } from 'react-i18next'
 import { useQuery } from '@tanstack/react-query'
 import { ProgramData } from '@backend/types'
 
-import { getSortedByName } from '../components/ThesisPage/util'
 import apiClient from '../util/apiClient'
 
 interface UseProgramsParams {
@@ -13,19 +12,19 @@ const usePrograms = (params: UseProgramsParams) => {
   const { i18n } = useTranslation()
   const { language } = i18n
 
-  const queryKey = ['programs', params?.includeNotManaged]
+  const queryKey = ['programs', params?.includeNotManaged, language]
 
   const queryFn = async (): Promise<ProgramData[]> => {
-    const { data } = await apiClient.get('/programs', { params })
+    const { data } = await apiClient.get('/programs', {
+      params: { ...params, language },
+    })
 
     return data
   }
 
   const { data: programs, ...rest } = useQuery({ queryKey, queryFn })
 
-  const sortedPrograms = getSortedByName(programs, language) as ProgramData[]
-
-  return { programs: sortedPrograms, ...rest }
+  return { programs, ...rest }
 }
 
 export default usePrograms
