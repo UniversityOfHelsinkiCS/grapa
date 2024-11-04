f70895f1b142881cfabf5aa2ca080c378670c4e7
Aleksandr Tereshchenko <aleksandr.tereshchenko@helsinki.fi>
2024-11-04 13:58:00 +0200

    /api/departments: Allow ordering based on selected language

--- src/server/routes/department.ts
+++ src/server/routes/department.ts
@@ -1,13 +1,22 @@
 import express, { Response } from 'express'
-import { Includeable } from 'sequelize'
+import { Includeable, QueryTypes } from 'sequelize'
 import { Department, DepartmentAdmin } from '../db/models'
 import { RequestWithUser } from '../types'
 
 const departmentRouter = express.Router()
 
+const getDepartmentQueryStr = (isAdmin: boolean, includeNotManaged: boolean) =>
+  `
+    SELECT departments.id, departments.name
+    FROM departments
+    ${!isAdmin && !includeNotManaged ? 'INNER JOIN department_admins ON departments.id = department_admins.department_id AND department_admins.user_id = $userId' : ''}
+    ORDER BY name->>$language ASC
+    `
+
 // @ts-expect-error the user middleware updates the req object with user field
 departmentRouter.get('/', async (req: RequestWithUser, res: Response) => {
   const { isAdmin } = req.user
+  const language = (req.query.language ?? 'en') as string
   const includeNotManaged = req.query.includeNotManaged === 'true'
 
   const includes: Includeable[] = []
@@ -21,11 +30,24 @@ departmentRouter.get('/', async (req: RequestWithUser, res: Response) => {
     })
   }
 
-  const departments = await Department.findAll({
-    attributes: ['id', 'name'],
-    include: includes,
-    order: [['name', 'ASC']],
-  })
+  // Validate that the language is one of the allowed keys
+  const allowedLanguages = ['en', 'fi', 'sv']
+  if (!allowedLanguages.includes(language)) {
+    throw new Error('Invalid language key')
+  }
+
+  const departments = await Department.sequelize?.query(
+    getDepartmentQueryStr(isAdmin, includeNotManaged),
+    {
+      bind: {
+        userId: req.user.id,
+        language,
+      },
+      type: QueryTypes.SELECT,
+      logging: console.log,
+    }
+  )
+
   res.send(departments)
 })
 
