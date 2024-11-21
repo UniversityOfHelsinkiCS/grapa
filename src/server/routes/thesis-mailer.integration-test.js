587470969087b6d62b4a2d6362d968f44447b235
Henri Remonen <henri@remonen.fi>
2024-11-21 14:35:44 +0200

    test: add more test cases

--- src/server/routes/thesis-mailer.integration-test.js
+++ src/server/routes/thesis-mailer.integration-test.js
@@ -352,4 +352,190 @@ describe('Theisis router with mocks', () => {
       })
     })
   })
+
+  describe('when the status is changed other than PLANNING to IN_PROGRESS', () => {
+    describe('when the user is an admin', () => {
+      it('setting status to COMPLETED should not call the sendEmail function and return 200', async () => {
+        const updatedThesis = {
+          programId: 'Updated program',
+          studyTrackId: 'new-test-study-track-id',
+          topic: 'Updated topic',
+          status: 'COMPLETED',
+          startDate: '1970-01-01T00:00:00.000Z',
+          targetDate: '2070-01-01T00:00:00.000Z',
+          supervisions: [
+            {
+              user: user1,
+              percentage: 100,
+              isPrimarySupervisor: true,
+            },
+          ],
+          authors: [user2],
+          graders: [
+            {
+              user: user3,
+              isPrimaryGrader: true,
+            },
+          ],
+          waysOfWorking: {
+            filename: 'testfile.pdf2',
+            name: 'testfile.pdf2',
+            mimetype: 'application/pdf2',
+          },
+          researchPlan: {
+            filename: 'testfile.pdf1',
+            name: 'testfile.pdf1',
+            mimetype: 'application/pdf1',
+          },
+        }
+
+        const response = await request
+          .put(`/api/theses/${thesis1.id}`)
+          .set('hygroupcn', 'grp-toska')
+          .field('json', JSON.stringify(updatedThesis))
+
+        expect(response.status).toEqual(200)
+        expect(sendEmail).toHaveBeenCalledTimes(0)
+      })
+
+      it('setting status to CANCELLED should not call the sendEmail function and return 200', async () => {
+        const updatedThesis = {
+          programId: 'Updated program',
+          studyTrackId: 'new-test-study-track-id',
+          topic: 'Updated topic',
+          status: 'CANCELLED',
+          startDate: '1970-01-01T00:00:00.000Z',
+          targetDate: '2070-01-01T00:00:00.000Z',
+          supervisions: [
+            {
+              user: user1,
+              percentage: 100,
+              isPrimarySupervisor: true,
+            },
+          ],
+          authors: [user2],
+          graders: [
+            {
+              user: user3,
+              isPrimaryGrader: true,
+            },
+          ],
+          waysOfWorking: {
+            filename: 'testfile.pdf2',
+            name: 'testfile.pdf2',
+            mimetype: 'application/pdf2',
+          },
+          researchPlan: {
+            filename: 'testfile.pdf1',
+            name: 'testfile.pdf1',
+            mimetype: 'application/pdf1',
+          },
+        }
+
+        const response = await request
+          .put(`/api/theses/${thesis1.id}`)
+          .set('hygroupcn', 'grp-toska')
+          .field('json', JSON.stringify(updatedThesis))
+
+        expect(response.status).toEqual(200)
+        expect(sendEmail).toHaveBeenCalledTimes(0)
+      })
+    })
+
+    describe('when the user is a program manager', () => {
+      beforeEach(async () => {
+        await ProgramManagement.create({
+          userId: programManagerUser.id,
+          programId: 'Testing program',
+          isThesisApprover: true,
+        })
+      })
+
+      it('setting status to COMPLETED should not call the sendEmail function and return 200', async () => {
+        const updatedThesis = {
+          programId: 'Testing program',
+          studyTrackId: 'test-study-track-id',
+          topic: 'test topic',
+          status: 'COMPLETED',
+          startDate: '1970-01-01',
+          targetDate: '2070-01-01',
+          supervisions: [
+            {
+              user: user1,
+              percentage: 100,
+              isPrimarySupervisor: true,
+            },
+          ],
+          authors: [user2],
+          graders: [
+            {
+              user: user3,
+              isPrimaryGrader: true,
+            },
+          ],
+          waysOfWorking: {
+            filename: 'testfile.pdf2',
+            name: 'testfile.pdf2',
+            mimetype: 'application/pdf2',
+          },
+          researchPlan: {
+            filename: 'testfile.pdf1',
+            name: 'testfile.pdf1',
+            mimetype: 'application/pdf1',
+          },
+        }
+
+        const response = await request
+          .put(`/api/theses/${thesis2.id}`)
+          .set({ uid: programManagerUser.id, hygroupcn: 'hy-employees' })
+          .field('json', JSON.stringify(updatedThesis))
+
+        expect(response.status).toEqual(200)
+        expect(sendEmail).toHaveBeenCalledTimes(0)
+      })
+
+      it('setting status to CANCELLED should not call the sendEmail function and return 200', async () => {
+        const updatedThesis = {
+          programId: 'Testing program',
+          studyTrackId: 'test-study-track-id',
+          topic: 'test topic',
+          status: 'CANCELLED',
+          startDate: '1970-01-01',
+          targetDate: '2070-01-01',
+          supervisions: [
+            {
+              user: user1,
+              percentage: 100,
+              isPrimarySupervisor: true,
+            },
+          ],
+          authors: [user2],
+          graders: [
+            {
+              user: user3,
+              isPrimaryGrader: true,
+            },
+          ],
+          waysOfWorking: {
+            filename: 'testfile.pdf2',
+            name: 'testfile.pdf2',
+            mimetype: 'application/pdf2',
+          },
+          researchPlan: {
+            filename: 'testfile.pdf1',
+            name: 'testfile.pdf1',
+            mimetype: 'application/pdf1',
+          },
+        }
+
+        const response = await request
+          .put(`/api/theses/${thesis2.id}`)
+          .set({ uid: programManagerUser.id, hygroupcn: 'hy-employees' })
+          .field('json', JSON.stringify(updatedThesis))
+
+        expect(response.status).toEqual(200)
+        expect(sendEmail).toHaveBeenCalledTimes(0)
+      })
+    })
+  })
 })
