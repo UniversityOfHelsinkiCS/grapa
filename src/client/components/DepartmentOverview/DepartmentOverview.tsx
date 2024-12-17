691e74f5c2794b9196f532b19a8c8996dc04b870
Henri Remonen <henri@remonen.fi>
2024-12-17 14:26:38 +0200

    feat: new component for department overview -refs: #206

---
+++ src/client/components/DepartmentOverview/DepartmentOverview.tsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from 'react'
+import {
+  CircularProgress,
+  Box,
+  Typography,
+  Select,
+  MenuItem,
+  ListItemText,
+  Stack,
+  InputLabel,
+  FormControl,
+} from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import { TranslationLanguage } from '@backend/types'
+import ThesesPage from '../ThesisPage/ThesesPage'
+import useDepartments from '../../hooks/useDepartments'
+
+const DepartmentOverview = () => {
+  const { t, i18n } = useTranslation()
+  const { language } = i18n as { language: TranslationLanguage }
+
+  const { departments, isLoading: departmentsAreLoading } = useDepartments({
+    includeNotManaged: false,
+  })
+  const [selectedDepartmentId, setSelectedDepartmentId] = useState<
+    string | null
+  >(null)
+
+  const handleChange = (targetDepartmentId: string) =>
+    setSelectedDepartmentId(targetDepartmentId)
+
+  useEffect(() => {
+    if (departments?.length > 0) {
+      setSelectedDepartmentId(departments[0].id)
+    }
+  }, [departments, setSelectedDepartmentId])
+
+  const selectedDepartment = departments?.find(
+    (department) => department.id === selectedDepartmentId
+  )
+
+  if (departmentsAreLoading || !departments?.length) {
+    return <CircularProgress />
+  }
+
+  return (
+    <Box component="section" sx={{ px: '1rem', py: '2rem', width: '100%' }}>
+      {selectedDepartment && departments?.length && (
+        <>
+          <FormControl sx={{ width: 500 }}>
+            <InputLabel id="department-select-label">
+              {t('departmentHeader')}
+            </InputLabel>
+            <Select
+              data-testid="department-select-input"
+              required
+              value={selectedDepartment.id}
+              id="DepartmentId"
+              labelId="department-select-label"
+              label={t('departmentHeader')}
+              name="DepartmentId"
+              onChange={(event) => {
+                handleChange(event.target.value)
+              }}
+              renderValue={(value) =>
+                departments.find((program) => program.id === value)?.name[
+                  language
+                ]
+              }
+            >
+              {departments.map((program) => (
+                <MenuItem
+                  data-testid={`department-select-item-${program.id}`}
+                  key={program.id}
+                  value={program.id}
+                >
+                  <ListItemText inset primary={program.name[language]} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </>
+      )}
+      {Boolean(selectedDepartment) && (
+        <>
+          <Stack sx={{ px: '1rem', py: '2rem' }}>
+            <Typography component="h1" variant="h4">
+              {t('theses')}
+            </Typography>
+            <ThesesPage
+              filteringDepartmentId={selectedDepartmentId}
+              noOwnThesesSwitch
+              noAddThesisButton
+            />
+          </Stack>
+        </>
+      )}
+    </Box>
+  )
+}
+
+export default DepartmentOverview
