2014d124beecab26698edef1552c44f812918b89
smxsm <74610181+e11-0a@users.noreply.github.com>
2026-05-11 15:39:36 +0300

    Move user search from studentRouter to service

--- src/server/routes/student.ts
+++ src/server/routes/student.ts
@@ -2,18 +2,10 @@ import express from 'express'
 
 import { RequestWithUser } from '../types'
 import withStudyRight from '../middleware/withStudyRight'
-import { Op } from 'sequelize'
-import { User } from '../db/models'
-import { restrictedUserFields } from './config'
 
-import {
-  getWhereClauseForManyWordSearch,
-  getWhereClauseForOneWordSearch,
-  getWhereClauseForTwoWordSearch,
-} from './usersSearchHelpers'
-
-const USER_FETCH_LIMIT = 100
 import { getPaginatedTheses } from '../services/thesisService'
+
+import { getUsersBySearch } from '../services/userService'
 
 const studentRouter = express.Router()
 
@@ -42,51 +34,8 @@ studentRouter.get('/theses', async (req: RequestWithUser, res: any) => {
 
 studentRouter.get('/users', async (req: RequestWithUser, res: any) => {
   const { search } = req.query
-
-  if (!search) {
-    res.status(400).send('Search string must be provided as a query parameter')
-    return
-  }
-
-  const trimmedSearch: string = search.trim()
-
-  if (trimmedSearch.length < 5) {
-    res.status(400).send('Search string must be at least 5 characters long')
-    return
-  }
-
-  let whereClauses: Record<string, any> = {
-    hasStudyRight: {
-      [Op.is]: true,
-    },
-  }
-
-  const searchedWords = trimmedSearch.split(' ')
-
-  if (searchedWords.length === 2) {
-    whereClauses = {
-      ...whereClauses,
-      ...getWhereClauseForTwoWordSearch(trimmedSearch),
-    }
-  } else if (searchedWords.length > 2) {
-    whereClauses = {
-      ...whereClauses,
-      ...getWhereClauseForManyWordSearch(trimmedSearch),
-    }
-  } else {
-    // the search consists of only one word
-    whereClauses = {
-      ...whereClauses,
-      ...getWhereClauseForOneWordSearch(trimmedSearch),
-    }
-  }
-
-  const users = await User.findAll({
-    attributes: restrictedUserFields,
-    where: whereClauses,
-    limit: USER_FETCH_LIMIT,
-  })
-  res.send(users)
+  const result = await getUsersBySearch(search as string)
+  res.send(result)
 })
 
 export default studentRouter
---
+++ src/server/services/userService.ts
@@ -0,0 +1,56 @@
+import { Op } from 'sequelize'
+import { User } from '../db/models'
+import { restrictedUserFields } from '../routes/config'
+
+import {
+  getWhereClauseForManyWordSearch,
+  getWhereClauseForOneWordSearch,
+  getWhereClauseForTwoWordSearch,
+} from '../routes/usersSearchHelpers'
+
+const USER_FETCH_LIMIT = 100
+
+export const getUsersBySearch = async (search: string) => {
+  if (!search) {
+    throw Error('Search string must be provided as a query parameter')
+  }
+
+  const trimmedSearch: string = search.trim()
+
+  if (trimmedSearch.length < 5) {
+    throw Error('Search string must be at least 5 characters long')
+  }
+
+  let whereClauses: Record<string, any> = {
+    hasStudyRight: {
+      [Op.is]: true,
+    },
+  }
+
+  const searchedWords = trimmedSearch.split(' ')
+
+  if (searchedWords.length === 2) {
+    whereClauses = {
+      ...whereClauses,
+      ...getWhereClauseForTwoWordSearch(trimmedSearch),
+    }
+  } else if (searchedWords.length > 2) {
+    whereClauses = {
+      ...whereClauses,
+      ...getWhereClauseForManyWordSearch(trimmedSearch),
+    }
+  } else {
+    // the search consists of only one word
+    whereClauses = {
+      ...whereClauses,
+      ...getWhereClauseForOneWordSearch(trimmedSearch),
+    }
+  }
+
+  const users = await User.findAll({
+    attributes: restrictedUserFields,
+    where: whereClauses,
+    limit: USER_FETCH_LIMIT,
+  })
+  return users
+}
