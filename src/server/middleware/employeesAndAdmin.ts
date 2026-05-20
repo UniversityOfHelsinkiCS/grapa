60c61e9deefa8bc703130ba2bf1227332c41b178
smxsm <74610181+e11-0a@users.noreply.github.com>
2026-05-20 23:03:14 +0300

    Exporting has_access from employeesAndAdminOnly to allow using the same implementation elsewhere

--- src/server/middleware/employeesAndAdmin.ts
+++ src/server/middleware/employeesAndAdmin.ts
@@ -1,16 +1,19 @@
 import CustomUnauthorizedError from '../errors/UnauthorizedError'
 import { NextFunction, Response } from 'express'
 
+export const has_access = (user: {
+  isAdmin: any
+  iamGroups: string | string[]
+}) => {
+  return user.isAdmin || user.iamGroups.includes('hy-employees')
+}
+
 export const employeesAndAdminOnly = async (
   req: any,
   _: Response,
   next: NextFunction
 ) => {
   const currentUser = req.user
-
-  const has_access = (user: { isAdmin: any; iamGroups: string | string[] }) => {
-    return user.isAdmin || user.iamGroups.includes('hy-employees')
-  }
 
   if (!currentUser || !currentUser.id || !has_access(currentUser)) {
     throw new CustomUnauthorizedError('Unauthorized')
