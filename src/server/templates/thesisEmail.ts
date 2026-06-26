30c78ea667c5cbb9d2368eadd5234c7497baa374
Markus <wahlman2002@gmail.com>
2026-06-26 15:55:42 +0300

    feat: translate all emails to finnish, english and swedish

--- src/server/templates/thesisEmail.ts
+++ src/server/templates/thesisEmail.ts
@@ -11,11 +11,20 @@ export const inProgressEmailTemplate = (
   actionUser: UserType
 ): TemplateOutput => {
   return {
-    subject: 'Prethesis - Thesis status changed to IN PROGRESS',
+    subject:
+      'Prethesis - Tutkielman tila muutettu: KÄYNNISSÄ / Thesis status changed to IN PROGRESS / Avhandlingens status ändrad: PÅGÅR',
     message: `
-    This is an automated message from Prethesis. \n\n
+Tämä on automaattinen viesti Prethesiksestä.
 
-    The status of the thesis "${updatedThesis.topic}" has been changed to IN PROGRESS by ${actionUser.firstName} ${actionUser.lastName}.
+Käyttäjä ${actionUser.firstName} ${actionUser.lastName} on muuttanut tutkielman "${updatedThesis.topic}" tilaksi KÄYNNISSÄ.
+---
+This is an automated message from Prethesis.
+
+The status of the thesis "${updatedThesis.topic}" has been changed to IN PROGRESS by ${actionUser.firstName} ${actionUser.lastName}.
+---
+Detta är ett automatiskt meddelande från Prethesis.
+
+Statusen för avhandlingen "${updatedThesis.topic}" har ändrats till PÅGÅR av ${actionUser.firstName} ${actionUser.lastName}.
   `,
   }
 }
@@ -29,25 +38,30 @@ export const ethesisSentEmailTemplate = (
   employeeTitlesSecond: { fi: string }
 ): TemplateOutput => {
   return {
-    subject: 'Prethesis - Tutkielma valmiina Ethesiskseen',
+    subject:
+      'Prethesis - Tutkielma valmiina Ethesikseen / Thesis ready for E-thesis / Avhandling redo för E-thesis',
     message: `
-    Seuraava tutkielma on valmiina siirrettäväksi Ethesikseen
+Seuraava tutkielma on valmiina siirrettäväksi Ethesikseen:
+The following thesis is ready to be transferred to E-thesis:
+Följande avhandling är redo att överföras till E-thesis:
 
-    ${author.firstName} ${author.lastName} (${author.studentNumber})
-    ${updatedThesis.topic}
+${author.firstName} ${author.lastName} (${author.studentNumber})
+${updatedThesis.topic}
 
-    ${program?.name?.fi || program?.name?.en} ${studyTrack ? `(${studyTrack.name?.fi || studyTrack.name?.en})` : ''}
+FI: ${program?.name?.fi || program?.name?.en} ${studyTrack ? `(${studyTrack.name?.fi || studyTrack.name?.en})` : ''}
+EN: ${program?.name?.en || program?.name?.fi} ${studyTrack ? `(${studyTrack.name?.en || studyTrack.name?.fi})` : ''}
+SV: ${program?.name?.sv || program?.name?.en} ${studyTrack ? `(${studyTrack.name?.sv || studyTrack.name?.en})` : ''}
 
-    Arvioijat:
+Arvioijat / Graders / Bedömare:
 
-    ${updatedThesis.graders
-      .map(
-        (grader) =>
-          `${grader.isPrimaryGrader ? employeeTitlesPrimer.fi : employeeTitlesSecond.fi} ${grader.user.firstName} ${grader.user.lastName} (${grader.user.email}) ${grader.isPrimaryGrader ? 'ensisijainen' : ''}`
-      )
-      .join('\n    ')}
+${updatedThesis.graders
+  .map(
+    (grader) =>
+      `${grader.isPrimaryGrader ? employeeTitlesPrimer.fi : employeeTitlesSecond.fi} ${grader.user.firstName} ${grader.user.lastName} (${grader.user.email}) ${grader.isPrimaryGrader ? '(ensisijainen / primary / primär)' : ''}`
+  )
+  .join('\n')}
 
-    <a href='https://prethesis.helsinki.fi/ethesis'>https://prethesis.helsinki.fi/ethesis</a>
+<a href='https://prethesis.helsinki.fi/ethesis'>https://prethesis.helsinki.fi/ethesis</a>
   `,
   }
 }
@@ -56,13 +70,26 @@ export const ethesisPermissionEmailTemplate = (
   updatedThesis: Thesis
 ): TemplateOutput => {
   return {
-    subject: 'Prethesis - Permission to submit to E-thesis',
+    subject:
+      'Prethesis - Lupa lähettää E-thesikseen / Permission to submit to E-thesis / Tillstånd att skicka in till E-thesis',
     message: `
-    This is an automated message from Prethesis. \n\n
+Tämä on automaattinen viesti Prethesiksestä.
 
-    You now have permission to submit your thesis "${updatedThesis.topic}" to E-thesis.
-    Please submit your thesis directly to E-thesis. You can find the instructions here:
-    <a href='https://studies.helsinki.fi/instructions/article/e-thesis'>https://studies.helsinki.fi/instructions/article/e-thesis</a>
+Sinulla on nyt lupa lähettää tutkielmasi "${updatedThesis.topic}" E-thesikseen.
+Ole hyvä ja lähetä tutkielmasi suoraan E-thesikseen. Löydät ohjeet täältä:
+<a href='https://studies.helsinki.fi/ohjeet/artikkeli/e-thesis'>https://studies.helsinki.fi/ohjeet/artikkeli/e-thesis</a>
+---
+This is an automated message from Prethesis.
+
+You now have permission to submit your thesis "${updatedThesis.topic}" to E-thesis.
+Please submit your thesis directly to E-thesis. You can find the instructions here:
+<a href='https://studies.helsinki.fi/instructions/article/e-thesis'>https://studies.helsinki.fi/instructions/article/e-thesis</a>
+---
+Detta är ett automatiskt meddelande från Prethesis.
+
+Du har nu tillåtelse att skicka in din avhandling "${updatedThesis.topic}" till E-thesis.
+Vänligen skicka in din avhandling direkt till E-thesis. Du hittar instruktionerna här:
+<a href='https://studies.helsinki.fi/instruktioner/artikel/e-thesis'>https://studies.helsinki.fi/instruktioner/artikel/e-thesis</a>
   `,
   }
 }
@@ -72,12 +99,23 @@ export const lastMilestoneReachedEmailTemplate = (
   actionUser: UserType
 ): TemplateOutput => {
   return {
-    subject: 'Prethesis - Last milestone reached',
+    subject:
+      'Prethesis - Viimeinen etappi saavutettu / Last milestone reached / Sista milstolpen nådd',
     message: `
-    This is an automated message from Prethesis. \n\n
+Tämä on automaattinen viesti Prethesiksestä.
 
-    The user ${actionUser.firstName} ${actionUser.lastName} has marked the last milestone as done for the thesis "${updatedThesis.topic}".
-    Please go to Prethesis, mark the second grader, and give the student permission to send the thesis to E-thesis.
+Käyttäjä ${actionUser.firstName} ${actionUser.lastName} on merkinnyt viimeisen etapin suoritetuksi tutkielmalle "${updatedThesis.topic}".
+Ole hyvä ja mene Prethesikseen, merkitse toinen arvioija ja anna opiskelijalle lupa lähettää tutkielma E-thesikseen.
+---
+This is an automated message from Prethesis.
+
+The user ${actionUser.firstName} ${actionUser.lastName} has marked the last milestone as done for the thesis "${updatedThesis.topic}".
+Please go to Prethesis, mark the second grader, and give the student permission to send the thesis to E-thesis.
+---
+Detta är ett automatiskt meddelande från Prethesis.
+
+Användaren ${actionUser.firstName} ${actionUser.lastName} har markerat den sista milstolpen som klar för avhandlingen "${updatedThesis.topic}".
+Vänligen gå till Prethesis, markera den andra bedömaren och ge studenten tillåtelse att skicka in avhandlingen till E-thesis.
   `,
   }
 }
@@ -87,13 +125,26 @@ export const newThesisToApproveEmailTemplate = (
   actionUser: UserType
 ): TemplateOutput => {
   return {
-    subject: 'Prethesis - A new thesis for you to approve',
+    subject:
+      'Prethesis - Uusi tutkielma hyväksyttäväksi / A new thesis to approve / En ny avhandling att godkänna',
     message: `
-    This is an automated message from Prethesis. \n\n
+Tämä on automaattinen viesti Prethesiksestä.
 
-    A new thesis "${newThesis.topic}" was created by ${actionUser.firstName} ${actionUser.lastName}.
-    The author of the thesis is ${newThesis.authors[0].firstName} ${newThesis.authors[0].lastName}.
-    You were marked as an approver for this thesis.
+Käyttäjä ${actionUser.firstName} ${actionUser.lastName} loi uuden tutkielman "${newThesis.topic}".
+Tutkielman tekijä on ${newThesis.authors[0].firstName} ${newThesis.authors[0].lastName}.
+Sinut on merkitty tutkielman hyväksyjäksi.
+---
+This is an automated message from Prethesis.
+
+A new thesis "${newThesis.topic}" was created by ${actionUser.firstName} ${actionUser.lastName}.
+The author of the thesis is ${newThesis.authors[0].firstName} ${newThesis.authors[0].lastName}.
+You were marked as an approver for this thesis.
+---
+Detta är ett automatiskt meddelande från Prethesis.
+
+En ny avhandling "${newThesis.topic}" skapades av ${actionUser.firstName} ${actionUser.lastName}.
+Författaren till avhandlingen är ${newThesis.authors[0].firstName} ${newThesis.authors[0].lastName}.
+Du markerades som godkännare för denna avhandling.
   `,
   }
 }
