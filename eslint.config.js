60cdd135cdd77cc4054762160e1655eaa7135311
Markus <wahlman2002@gmail.com>
2026-07-01 14:31:38 +0300

    chore: add eslint rule to ignore unused with underscore

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,14 @@ export default [
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
     },
     languageOptions: {
       parserOptions: {
