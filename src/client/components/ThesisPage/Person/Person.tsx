1012cb6d12bf216508a5cc600eee20ad4fe5f22f
smxsm <74610181+e11-0a@users.noreply.github.com>
2026-07-13 10:20:23 +0300

    Add Person and PersonList components

---
+++ src/client/components/ThesisPage/Person/Person.tsx
@@ -0,0 +1,63 @@
+import { User } from '@backend/types'
+import { Stack, Typography } from '@mui/material'
+
+export const Person = ({
+  user,
+  percentage,
+  title,
+  showStudentNumber,
+}: {
+  user: User
+  percentage?: number
+  title?: string
+  showStudentNumber?: boolean
+}) => {
+  const primaryText =
+    `${user.firstName} ${user.lastName}` +
+    (percentage != undefined ? ` (${percentage}%)` : '')
+  const secondaryText = user.affiliation
+    ? `${user.email} (${user.affiliation})`
+    : `${user.email}`
+  return (
+    <>
+      <Stack direction="column" sx={{ gap: 0.3 }}>
+        <Typography
+          component="span"
+          sx={{ lineHeight: 1.25, fontSize: '0.875rem', fontWeight: 500 }}
+        >
+          {primaryText}
+          {showStudentNumber && user.studentNumber && (
+            <Typography
+              sx={{
+                fontFamily: 'monospace',
+                lineHeight: 1.25,
+                fontSize: '0.8rem',
+                fontWeight: 300,
+              }}
+            >
+              {user.studentNumber}
+            </Typography>
+          )}
+        </Typography>
+        {title && (
+          <Typography
+            sx={{
+              lineHeight: 1.25,
+              fontSize: '0.8rem',
+              fontWeight: 300,
+            }}
+          >
+            {title}
+          </Typography>
+        )}
+        <Typography
+          sx={{ fontSize: '10pt', lineHeight: 1, color: '#005a94' }}
+          component="a"
+          href={'mailto:' + user.email}
+        >
+          {secondaryText}
+        </Typography>
+      </Stack>
+    </>
+  )
+}
---
+++ src/client/components/ThesisPage/Person/PersonList.tsx
@@ -0,0 +1,93 @@
+import { useTranslation } from 'react-i18next'
+import { Stack, Typography, Tooltip, IconButton } from '@mui/material'
+
+import { TranslatedName, User } from '@backend/types'
+
+import { ContentCopy } from '@mui/icons-material'
+import { Person } from './Person'
+
+interface UserContainer {
+  user: Partial<User>
+  percentage?: number
+  isExternal?: boolean
+  isPrimarySupervisor?: boolean
+  title?: TranslatedName
+  creationTimeIdentifier?: string
+}
+
+export const PersonList = ({
+  users,
+  title,
+  showStudentNumber,
+  showTitle,
+}: {
+  users: UserContainer[]
+  title: string
+  showStudentNumber?: boolean
+  showTitle?: boolean
+}) => {
+  if (!users.length) return null
+
+  const { t, i18n } = useTranslation()
+  const { language } = i18n
+
+  return (
+    <Stack
+      sx={{
+        minWidth: '15rem',
+      }}
+    >
+      <Stack direction="row" sx={{ alignItems: 'center' }}>
+        <Typography
+          component="h4"
+          sx={{
+            fontSize: '1rem',
+            fontWeight: 700,
+            textTransform: 'uppercase',
+          }}
+        >
+          {title}
+        </Typography>
+        <Tooltip title={t('common:copyEmails')}>
+          <IconButton
+            sx={{
+              ml: 'auto',
+              opacity: 0.5,
+              ':hover': {
+                opacity: 1,
+              },
+            }}
+            onClick={() => {
+              void navigator.clipboard.writeText(
+                users
+                  .map((userContainer) => userContainer.user.email)
+                  .filter((email) => email)
+                  .join('\n')
+              )
+            }}
+          >
+            <ContentCopy />
+          </IconButton>
+        </Tooltip>
+      </Stack>
+      <Stack sx={{ gap: 1.5, my: 1.5 }}>
+        {users.map((userContainer) => {
+          const title =
+            userContainer.title?.fi && userContainer.title?.fi.length > 0
+              ? `(${userContainer.title[language as keyof TranslatedName]})`
+              : ''
+
+          return (
+            <Person
+              key={userContainer.user.id}
+              user={userContainer.user as unknown as User}
+              showStudentNumber={showStudentNumber}
+              percentage={userContainer.percentage}
+              title={showTitle ? title : null}
+            ></Person>
+          )
+        })}
+      </Stack>
+    </Stack>
+  )
+}
