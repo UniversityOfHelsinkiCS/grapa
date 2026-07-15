f951dd4135a7fe163c3c690ac76ced84d2632b86
Markus <wahlman2002@gmail.com>
2026-07-15 16:28:05 +0300

    refactor: use PrethesisTable for statistics

--- src/client/components/ManageEntity/Statistics.tsx
+++ src/client/components/ManageEntity/Statistics.tsx
@@ -1,16 +1,8 @@
 import { useTranslation } from 'react-i18next'
-import { Box, Tooltip, Typography } from '@mui/material'
-import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid'
-
-import { fiFI, enUS } from '@mui/x-data-grid/locales'
-
-import {
-  ThesisStatistics,
-  ThesisStatus,
-  TranslationLanguage,
-} from '@backend/types'
+import { Box, Typography } from '@mui/material'
 
 import { useThesisStatistics } from '../../hooks/useTheses'
+import StatisticsTable from './StatisticsTable'
 
 interface Props {
   filteringDepartmentId?: string
@@ -25,8 +17,7 @@ const Statistics = ({
   studyTrackId,
   hideTitle,
 }: Props) => {
-  const { t, i18n } = useTranslation()
-  const { language } = i18n as { language: TranslationLanguage }
+  const { t } = useTranslation()
 
   const { thesisStatistics, isLoading: thesisStatisticsLoading } =
     useThesisStatistics({
@@ -36,215 +27,6 @@ const Statistics = ({
     })
 
   if (thesisStatisticsLoading || !thesisStatistics) return null
-
-  const filteredDepartmentStatistics = thesisStatistics
-
-  const totalThesisCounts = filteredDepartmentStatistics.reduce(
-    (acc, { statusCounts, startedWithinHalfYearCount }) => {
-      ;(Object.entries(statusCounts) as [ThesisStatus, number][]).forEach(
-        ([status, count]) => {
-          acc[status] = (acc[status] || 0) + count
-        }
-      )
-      acc.startedWithinHalfYearCount =
-        (acc.startedWithinHalfYearCount || 0) + startedWithinHalfYearCount
-      return acc
-    },
-    {
-      startedWithinHalfYearCount: 0,
-      DRAFT: 0,
-      SUGGESTED: 0,
-      PLANNING: 0,
-      IN_PROGRESS: 0,
-      COMPLETED: 0,
-      CANCELLED: 0,
-    } as { startedWithinHalfYearCount: number } & Record<ThesisStatus, number>
-  )
-
-  const dataGridLocale = language === 'fi' ? fiFI : enUS
-
-  const columns: GridColDef<ThesisStatistics>[] = [
-    {
-      field: 'supervisor',
-      headerName: t('departmentStatisticsPage:supervisorHeader'),
-      headerAlign: 'left',
-      flex: 1,
-      valueGetter: ({ firstName, lastName, email }) =>
-        `${lastName} ${firstName} ${email ? ` (${email})` : ''}`,
-    },
-    {
-      field: 'department',
-      headerName: t('departmentStatisticsPage:departmentHeader'),
-      headerAlign: 'left',
-      flex: 1,
-      valueGetter: (_, { department }) => department?.name?.[language] || '-',
-    },
-    {
-      field: 'thesisCount.DRAFT',
-      headerName: t('thesisStages:draft'),
-      renderHeader: () => (
-        <Typography variant="body2">
-          {t('thesisStages:draft') + ` (${totalThesisCounts.DRAFT})`}
-        </Typography>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { statusCounts }) => statusCounts.DRAFT,
-    },
-    {
-      field: 'thesisCount.SUGGESTED',
-      headerName: t('thesisStages:suggested'),
-      renderHeader: () => (
-        <Typography variant="body2">
-          {t('thesisStages:suggested') + ` (${totalThesisCounts.SUGGESTED})`}
-        </Typography>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { statusCounts }) => statusCounts.SUGGESTED,
-    },
-    {
-      field: 'thesisCount.PLANNING',
-      headerName: t('thesisStages:planned'),
-      renderHeader: () => (
-        <Typography variant="body2">
-          {t('thesisStages:planned') + ` (${totalThesisCounts.PLANNING})`}
-        </Typography>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { statusCounts }) => statusCounts.PLANNING,
-    },
-    {
-      field: 'thesisCount.IN_PROGRESS',
-      headerName: t('thesisStages:inProgress'),
-      renderHeader: () => (
-        <Typography variant="body2">
-          {t('thesisStages:inProgress') + ` (${totalThesisCounts.IN_PROGRESS})`}
-        </Typography>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { statusCounts }) => statusCounts.IN_PROGRESS,
-    },
-    {
-      field: 'thesisCount.COMPLETED',
-      headerName: t('thesisStages:completed'),
-      renderHeader: () => (
-        <Typography variant="body2">
-          {t('thesisStages:completed') + ` (${totalThesisCounts.COMPLETED})`}
-        </Typography>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { statusCounts }) => statusCounts.COMPLETED,
-    },
-    {
-      field: 'thesisCount.CANCELLED',
-      headerName: t('thesisStages:cancelled'),
-      renderHeader: () => (
-        <Typography variant="body2">
-          {t('thesisStages:cancelled') + ` (${totalThesisCounts.CANCELLED})`}
-        </Typography>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { statusCounts }) => statusCounts.CANCELLED,
-    },
-    {
-      field: 'thesisCount.STARTED_WITHIN_HALF_YEAR',
-      headerName: t('departmentStatisticsPage:startedWithinHalfYearCount'),
-      renderHeader: () => (
-        <Tooltip
-          title={t('departmentStatisticsPage:startedWithinHalfYearTooltip')}
-        >
-          <Typography variant="body2">
-            {t('departmentStatisticsPage:startedWithinHalfYearCount') +
-              ` (${totalThesisCounts.startedWithinHalfYearCount})`}
-          </Typography>
-        </Tooltip>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { startedWithinHalfYearCount }) =>
-        startedWithinHalfYearCount,
-    },
-    {
-      field: 'thesisCount.LATE_COUNT',
-      headerName: t('departmentStatisticsPage:late'),
-      renderHeader: () => (
-        <Tooltip title={t('departmentStatisticsPage:lateTooltip')}>
-          <Typography variant="body2">
-            {t('departmentStatisticsPage:late')}
-          </Typography>
-        </Tooltip>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { lateSupervisionsCount }) =>
-        lateSupervisionsCount != 0 ? lateSupervisionsCount : null,
-    },
-    {
-      field: 'thesisCount.LATE_TIME',
-      headerName: t('departmentStatisticsPage:avgLate'),
-      renderHeader: () => (
-        <Tooltip title={t('departmentStatisticsPage:avgLateTooltip')}>
-          <Typography variant="body2">
-            {t('departmentStatisticsPage:avgLate')}
-          </Typography>
-        </Tooltip>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { avgLateSupervision }) =>
-        avgLateSupervision != 0 ? `${Math.round(avgLateSupervision)}` : null,
-    },
-    {
-      field: 'thesisCount.COMPLETE_TIME',
-      headerName: t('departmentStatisticsPage:avgCompletion'),
-      renderHeader: () => (
-        <Tooltip title={t('departmentStatisticsPage:avgCompletionTooltip')}>
-          <Typography variant="body2">
-            {t('departmentStatisticsPage:avgCompletion')}
-          </Typography>
-        </Tooltip>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { avgCompletedSupervision }) =>
-        avgCompletedSupervision != 0
-          ? `${Math.round(avgCompletedSupervision)}`
-          : null,
-    },
-    {
-      field: 'thesisCount.PRIMARY_SUPERVISED',
-      headerName: t('departmentStatisticsPage:primarySupervisions'),
-      renderHeader: () => (
-        <Tooltip
-          title={t('departmentStatisticsPage:primarySupervisionsTooltip')}
-        >
-          <Typography variant="body2">
-            {t('departmentStatisticsPage:primarySupervisions')}
-          </Typography>
-        </Tooltip>
-      ),
-      filterable: false,
-      width: 150,
-      type: 'number',
-      valueGetter: (_, { primarySupervisionsCount }) =>
-        primarySupervisionsCount,
-    },
-  ]
 
   return (
     <Box
@@ -266,35 +48,19 @@ const Statistics = ({
           {t('departmentStatisticsPage:pageTitle')}
         </Typography>
       )}
-      <DataGrid
-        sx={{ mt: hideTitle ? 0 : '2rem' }}
-        rows={filteredDepartmentStatistics}
-        columns={columns}
-        getRowId={(row) => row.supervisor.id}
-        showToolbar={true}
-        slots={{ toolbar: GridToolbar }}
-        localeText={
-          dataGridLocale.components.MuiDataGrid.defaultProps.localeText
-        }
-        columnGroupingModel={[
-          {
-            groupId: t('departmentStatisticsPage:thesisCountHeader'),
-            headerAlign: 'center',
-            children: [
-              { field: 'thesisCount.DRAFT' },
-              { field: 'thesisCount.SUGGESTED' },
-              { field: 'thesisCount.PLANNING' },
-              { field: 'thesisCount.IN_PROGRESS' },
-              { field: 'thesisCount.COMPLETED' },
-              { field: 'thesisCount.CANCELLED' },
-              { field: 'thesisCount.STARTED_WITHIN_HALF_YEAR' },
-            ],
-          },
-        ]}
-        // we cannot disable pagination in the community version
-        // so we set the page size to a high number
-        pageSizeOptions={[2000]}
-      />
+      <Box
+        sx={{
+          mt: hideTitle ? 0 : '2rem',
+          border: 1,
+          borderColor: 'divider',
+          borderRadius: 1,
+        }}
+      >
+        <StatisticsTable
+          statistics={thesisStatistics}
+          isLoading={thesisStatisticsLoading}
+        />
+      </Box>
     </Box>
   )
 }
---
+++ src/client/components/ManageEntity/StatisticsTable.tsx
@@ -0,0 +1,250 @@
+import React, { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Tooltip, Typography } from '@mui/material'
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+
+import {
+  ThesisStatistics,
+  ThesisStatus,
+  TranslationLanguage,
+} from '@backend/types'
+
+import PrethesisTable from '../Common/PrethesisTable'
+
+interface Props {
+  statistics: ThesisStatistics[]
+  isLoading?: boolean
+}
+
+const columnHelper = createColumnHelper<ThesisStatistics>()
+
+const StatisticsTable = ({ statistics, isLoading }: Props) => {
+  const { t, i18n } = useTranslation()
+  const { language } = i18n as { language: TranslationLanguage }
+
+  const totalThesisCounts = useMemo(() => {
+    return statistics.reduce(
+      (acc, { statusCounts, startedWithinHalfYearCount }) => {
+        ;(Object.entries(statusCounts) as [ThesisStatus, number][]).forEach(
+          ([status, count]) => {
+            acc[status] = (acc[status] || 0) + count
+          }
+        )
+        acc.startedWithinHalfYearCount =
+          (acc.startedWithinHalfYearCount || 0) + startedWithinHalfYearCount
+        return acc
+      },
+      {
+        startedWithinHalfYearCount: 0,
+        DRAFT: 0,
+        SUGGESTED: 0,
+        PLANNING: 0,
+        IN_PROGRESS: 0,
+        COMPLETED: 0,
+        CANCELLED: 0,
+      } as { startedWithinHalfYearCount: number } & Record<ThesisStatus, number>
+    )
+  }, [statistics])
+
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor('supervisor', {
+        id: 'supervisor',
+        header: t('departmentStatisticsPage:supervisorHeader'),
+        cell: (info) => {
+          const { firstName, lastName, email } = info.getValue()
+          return `${lastName} ${firstName} ${email ? ` (${email})` : ''}`
+        },
+        sortingFn: (rowA, rowB) => {
+          const a = `${rowA.original.supervisor.lastName} ${rowA.original.supervisor.firstName}`
+          const b = `${rowB.original.supervisor.lastName} ${rowB.original.supervisor.firstName}`
+          return a.localeCompare(b)
+        },
+      }),
+      columnHelper.accessor('department', {
+        id: 'department',
+        header: t('departmentStatisticsPage:departmentHeader'),
+        cell: (info) => info.getValue()?.name?.[language] || '-',
+      }),
+      columnHelper.group({
+        header: t('departmentStatisticsPage:thesisCountHeader'),
+        id: 'thesisCount',
+        columns: [
+          columnHelper.accessor('statusCounts.DRAFT', {
+            id: 'DRAFT',
+            header: () => (
+              <Typography
+                variant="body2"
+                sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+              >
+                {t('thesisStages:draft') + ` (${totalThesisCounts.DRAFT})`}
+              </Typography>
+            ),
+            cell: (info) => info.getValue(),
+          }),
+          columnHelper.accessor('statusCounts.SUGGESTED', {
+            id: 'SUGGESTED',
+            header: () => (
+              <Typography
+                variant="body2"
+                sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+              >
+                {t('thesisStages:suggested') +
+                  ` (${totalThesisCounts.SUGGESTED})`}
+              </Typography>
+            ),
+            cell: (info) => info.getValue(),
+          }),
+          columnHelper.accessor('statusCounts.PLANNING', {
+            id: 'PLANNING',
+            header: () => (
+              <Typography
+                variant="body2"
+                sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+              >
+                {t('thesisStages:planned') + ` (${totalThesisCounts.PLANNING})`}
+              </Typography>
+            ),
+            cell: (info) => info.getValue(),
+          }),
+          columnHelper.accessor('statusCounts.IN_PROGRESS', {
+            id: 'IN_PROGRESS',
+            header: () => (
+              <Typography
+                variant="body2"
+                sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+              >
+                {t('thesisStages:inProgress') +
+                  ` (${totalThesisCounts.IN_PROGRESS})`}
+              </Typography>
+            ),
+            cell: (info) => info.getValue(),
+          }),
+          columnHelper.accessor('statusCounts.COMPLETED', {
+            id: 'COMPLETED',
+            header: () => (
+              <Typography
+                variant="body2"
+                sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+              >
+                {t('thesisStages:completed') +
+                  ` (${totalThesisCounts.COMPLETED})`}
+              </Typography>
+            ),
+            cell: (info) => info.getValue(),
+          }),
+          columnHelper.accessor('statusCounts.CANCELLED', {
+            id: 'CANCELLED',
+            header: () => (
+              <Typography
+                variant="body2"
+                sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+              >
+                {t('thesisStages:cancelled') +
+                  ` (${totalThesisCounts.CANCELLED})`}
+              </Typography>
+            ),
+            cell: (info) => info.getValue(),
+          }),
+          columnHelper.accessor('startedWithinHalfYearCount', {
+            id: 'STARTED_WITHIN_HALF_YEAR',
+            header: () => (
+              <Tooltip
+                title={t(
+                  'departmentStatisticsPage:startedWithinHalfYearTooltip'
+                )}
+              >
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+                >
+                  {t('departmentStatisticsPage:startedWithinHalfYearCount') +
+                    ` (${totalThesisCounts.startedWithinHalfYearCount})`}
+                </Typography>
+              </Tooltip>
+            ),
+            cell: (info) => info.getValue(),
+          }),
+        ],
+      }),
+      columnHelper.accessor('lateSupervisionsCount', {
+        id: 'LATE_COUNT',
+        header: () => (
+          <Tooltip title={t('departmentStatisticsPage:lateTooltip')}>
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+            >
+              {t('departmentStatisticsPage:late')}
+            </Typography>
+          </Tooltip>
+        ),
+        cell: (info) => (info.getValue() !== 0 ? info.getValue() : null),
+      }),
+      columnHelper.accessor('avgLateSupervision', {
+        id: 'LATE_TIME',
+        header: () => (
+          <Tooltip title={t('departmentStatisticsPage:avgLateTooltip')}>
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+            >
+              {t('departmentStatisticsPage:avgLate')}
+            </Typography>
+          </Tooltip>
+        ),
+        cell: (info) =>
+          info.getValue() !== 0 ? Math.round(info.getValue()) : null,
+      }),
+      columnHelper.accessor('avgCompletedSupervision', {
+        id: 'COMPLETE_TIME',
+        header: () => (
+          <Tooltip title={t('departmentStatisticsPage:avgCompletionTooltip')}>
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+            >
+              {t('departmentStatisticsPage:avgCompletion')}
+            </Typography>
+          </Tooltip>
+        ),
+        cell: (info) =>
+          info.getValue() !== 0 ? Math.round(info.getValue()) : null,
+      }),
+      columnHelper.accessor('primarySupervisionsCount', {
+        id: 'PRIMARY_SUPERVISED',
+        header: () => (
+          <Tooltip
+            title={t('departmentStatisticsPage:primarySupervisionsTooltip')}
+          >
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+            >
+              {t('departmentStatisticsPage:primarySupervisions')}
+            </Typography>
+          </Tooltip>
+        ),
+        cell: (info) => info.getValue(),
+      }),
+    ],
+    [language, t, totalThesisCounts]
+  )
+
+  const table = useReactTable({
+    data: statistics,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getRowId: (row) => row.supervisor.id,
+  })
+
+  return <PrethesisTable table={table} isLoading={isLoading} />
+}
+
+export default StatisticsTable
