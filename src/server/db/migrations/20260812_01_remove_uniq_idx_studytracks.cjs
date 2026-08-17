be8978c37eb2dd067541dacd856c45876e61728f
smxsm <74610181+e11-0a@users.noreply.github.com>
2026-08-17 12:05:11 +0300

    migrations

---
+++ src/server/db/migrations/20260812_01_remove_uniq_idx_studytracks.cjs
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async queryInterface => {
+    await queryInterface.removeIndex('study_tracks', 'study_tracks_name_program_id_unique_index')
+    // await queryInterface.addIndex('study_tracks', ['name', 'program_id', 'sisu_id'], {
+    //   unique: true,
+    //   name: 'study_tracks_name_program_id_unique_index',
+    // })
+  },
+  down: async queryInterface => {
+    // await queryInterface.removeIndex('study_tracks', 'study_tracks_name_program_id_unique_index')
+    await queryInterface.addIndex('study_tracks', ['name', 'program_id'], {
+      unique: true,
+      name: 'study_tracks_name_program_id_unique_index',
+    })
+}
+}
